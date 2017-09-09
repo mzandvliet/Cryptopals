335745417aa869c74c41ae5f5d234eba262e9693
m_zandvliet <martijn@ramjetanvil.com>
2017-09-09 19:33:07 +0200

    BAM! Challenge 6 busted.

--- Cryptopals/Cryptopals.py
+++ Cryptopals/Cryptopals.py
@@ -1,6 +1,7 @@
 import codecs
 import binascii
 import base64
+from functools import reduce
 
 """ 
 # plaintext has been put through:
@@ -17,6 +18,9 @@ Todo: It'll be interesting to convert this code to numpy and/or tensorflow later
 
 def strToByteArray(str):
     return bytearray(str.encode('utf-8'))
+
+def byteArrayToStr(bytes):
+    return ''.join(chr(i) for i in bytes)
 
 def distHamming(lineA, lineB):
     """
@@ -49,6 +53,43 @@ def testHamming():
 
     assert(distHamming(lineA, lineB) == 37)
 
+def xor_single_char(bytes, char):
+    result = []
+    for i in range(len(bytes)):
+        result.append(bytes[i] ^ char)
+    return bytearray(result)
+
+def rate_englishness(bytes):
+    garbage_count = 0
+    for i in bytes:
+        if is_garbage(i):
+            garbage_count+=1
+    return len(bytes)-garbage_count
+
+def is_garbage(byte):
+    if byte == 32: return False #space
+    if byte >= 65 and byte <= 90: return False #uppercase
+    if byte >= 97 and byte <= 122: return False #lowercase
+    return True
+
+def most_likely_keychar(bytes):
+    """
+    returns (key, keyscore)
+    """
+    key = None
+    key_score = 0
+    for char in range(255):
+        decoded = xor_single_char(bytes, char)
+        score = rate_englishness(decoded)
+        if score > key_score: # note: multiple keys with same good score possible
+            key = char
+            key_score = score
+
+    return (key, key_score)
+
+def rotXor(bytes, key):
+    return [bytes[i] ^ key[i % len(key)] for i in range(len(bytes))]
+
 if __name__ == '__main__':
     testHamming()
 
@@ -58,8 +99,8 @@ if __name__ == '__main__':
     file.close()
 
     # Try all key sizes, compute average hamming norms
-    candidate_sizes = []
-    for keysize in range(2, 40):
+    candidateKeySizes = []
+    for keysize in range(27, 31):
         count = 0
         totalDist = 0
         
@@ -73,24 +114,46 @@ if __name__ == '__main__':
             totalDist += distHamming(bytesA, bytesB)
 
         avgNormDist = totalDist/(count * keysize)
-        candidate_sizes.append((keysize, avgNormDist))
+        candidateKeySizes.append((keysize, avgNormDist))
         
         print("Key Size: %s, avg norm dist: %s"%(keysize, avgNormDist))
     
-    candidate_sizes.sort(key=lambda tup:tup[1])
+    candidateKeySizes.sort(key=lambda tup:tup[1])
 
     print("Most likely key sizes:")
-    for i in candidate_sizes[:5]:
+    for i in candidateKeySizes[:2]:
         print(" - %s, score: %s"%(i[0], i[1]))
 
-    candidate_sizes = candidate_sizes[:5]
+    candidateKeySizes = candidateKeySizes[:2]
 
-    for candidate in candidate_sizes:
+    candidate_keys = []
+    for candidate in candidateKeySizes:
         keysize = candidate[0]
         blocks = [[] for i in range(keysize)]
-        for i in range(0, len(text), keysize):
+        for i in range(0, len(text)):
             blocks[i%keysize].append(text[i])
 
+        keyChars = []
+        for block in blocks:
+            keyChars.append(most_likely_keychar(block))
+        key = []
+        keyScore = 0 # todo: as comprehension
+        for i in keyChars:
+            key.append(i[0])
+            keyScore += i[1]
 
+        candidate_keys.append((key, keyScore))
 
-    print("bla")
+    candidate_keys.sort(key=lambda tup:tup[1], reverse=True)
+    
+    for key in candidate_keys:
+        print("key: %s, score: %s"%(byteArrayToStr(key[0]), key[1]))
+
+    key = candidate_keys[0][0]
+    decryptedBytes = rotXor(text, key)
+    decrypted_str = byteArrayToStr(decryptedBytes)
+
+    print("Done! Message: \n")
+    print(decrypted_str)
+
+    
